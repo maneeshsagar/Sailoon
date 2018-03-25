406db95e8f252a38226a608b5edbb1667348c37d
mayankpshahi <mayankpshahi239@gmail.com>
2018-03-25 15:57:27 +0530

    Login page added

--- app/src/main/java/com/example/maneeshsagar/sailoon/MainActivity.java
+++ app/src/main/java/com/example/maneeshsagar/sailoon/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.maneeshsagar.sailoon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login=(Button)findViewById(R.id.button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Login Sucessfully",Toast.LENGTH_LONG).show();
+            }
+        });
 
 
     }
